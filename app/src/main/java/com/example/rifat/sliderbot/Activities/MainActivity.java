a40869a8d1f853655e8980232b931f1aab617a68
BladeRunner1988 <meer.m.zaman@gmail.com>
2015-07-29 15:32:42 +0600

    Full App Updated. Added folders for differentiating activities and utilities. New tabbed activity for How to Use Manual.

--- app/src/main/java/com/example/rifat/sliderbot/Activities/MainActivity.java
+++ app/src/main/java/com/example/rifat/sliderbot/Activities/MainActivity.java
@@ -1,23 +1,25 @@
-package com.example.rifat.sliderbot;
+package com.example.rifat.sliderbot.Activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.rifat.sliderbot.R;
 
-public class SettingsActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
