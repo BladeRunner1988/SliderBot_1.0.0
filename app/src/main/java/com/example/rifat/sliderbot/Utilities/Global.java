cc1f9903013440eaff720ee19212f381777e6c6d
BladeRunner1988 <meer.m.zaman@gmail.com>
2015-08-02 20:03:31 +0600

    Corrected Button click events. Corrected theme. Added Splash Screen. Added Application Icon.

--- app/src/main/java/com/example/rifat/sliderbot/Utilities/Global.java
+++ app/src/main/java/com/example/rifat/sliderbot/Utilities/Global.java
@@ -8,7 +8,8 @@ import java.util.ArrayList;
  * Created by Rifat on 7/29/2015.
  */
 public class Global extends Application {
-    private ArrayList<MySliderPresets> myPresets;
+    private ArrayList myPresets;
+    private boolean motion; // If this is false then backwardMotion and if true then forwardMotion
 
     public ArrayList<MySliderPresets> getMyPresets() {
         return myPresets;
@@ -16,5 +17,13 @@ public class Global extends Application {
 
     public void setMyPresets(ArrayList<MySliderPresets> myPresets) {
         this.myPresets = myPresets;
+    }
+
+    public boolean isMotion() {
+        return motion;
+    }
+
+    public void setMotion(boolean motion) {
+        this.motion = motion;
     }
 }
