edd8ffd37a9f56a0be5eb0d7f6f93eab8ee01514
BladeRunner1988 <meer.m.zaman@gmail.com>
2015-08-02 21:08:21 +0600

    New files added to the project

--- app/src/main/java/com/example/rifat/sliderbot/Activities/MainActivity.java
+++ app/src/main/java/com/example/rifat/sliderbot/Activities/MainActivity.java
@@ -1,23 +1,41 @@
 package com.example.rifat.sliderbot.Activities;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.rifat.sliderbot.R;
 import com.example.rifat.sliderbot.Utilities.Global;
 import com.example.rifat.sliderbot.Utilities.MySliderPresets;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    ListView lvBluetoothDevices;
 
     Button btnSettings,
             btnHowToUse,
@@ -27,14 +45,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnSelectPresets,
             btnEditTime,
             btnEditTravel,
-            btnGo;
+            btnGo,
+            btnConnectBluetooth,
+            btnCancelConnection,
+            btnConnectToBluetoothDevice;
 
+    EditText etTime, etTravel;
 
+    private String time = "";
+    private String travel = "";
 
+    Drawable defaultBackground;
 
-    ArrayList<MySliderPresets> myPresets = ((Global)this.getApplication()).getMyPresets();
+    Global global;
+    ArrayList<MySliderPresets> myPresets;
+    boolean motionDirection;
 
-    private static final String MyPreferences = "MyPrefs";
+    private static String MyPreferences;
     SharedPreferences sharedPreferences;
 
     @Override
@@ -46,6 +73,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void init() {
+        MyPreferences = "MyPrefs";
+
+        global = (Global)getApplication();
+        myPresets = global.getMyPresets();
+        motionDirection = global.isMotion();
+
+        lvBluetoothDevices = (ListView) findViewById(R.id.lvBluetoothDevices);
+
+        etTime = (EditText) findViewById(R.id.etTime);
+        etTravel = (EditText) findViewById(R.id.etTravel);
+
         btnSettings = (Button) findViewById(R.id.btnSettings);
         btnHowToUse = (Button) findViewById(R.id.btnHowToUse);
         btnAbout = (Button) findViewById(R.id.btnAbout);
@@ -55,6 +93,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnEditTime = (Button) findViewById(R.id.btnEditTime);
         btnEditTravel = (Button) findViewById(R.id.btnEditTravel);
         btnGo = (Button) findViewById(R.id.btnGo);
+        btnConnectBluetooth = (Button) findViewById(R.id.btnConnectBluetooth);
+        btnCancelConnection = (Button) findViewById(R.id.btnCancelConnection);
+        btnConnectToBluetoothDevice = (Button) findViewById(R.id.btnConnectToBluetoothDevice);
 
         btnSettings.setOnClickListener(this);
         btnHowToUse.setOnClickListener(this);
@@ -66,16 +107,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnEditTravel.setOnClickListener(this);
         btnGo.setOnClickListener(this);
 
-        populatePresetList();
+        //populatePresetList();
     }
 
     private void populatePresetList() {
         sharedPreferences = getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
         for(int i=0;i<5;i++) {
-            String name = sharedPreferences.getString("presetName"+i, "Default");
+            String name = sharedPreferences.getString("presetName" + i, "Default");
             int time = sharedPreferences.getInt("presetTime"+i, 0);
-            int travel = sharedPreferences.getInt("presetTravel"+i, 0);
-            myPresets.add(new MySliderPresets(name, time, travel));
+            int travel = sharedPreferences.getInt("presetTravel" + i, 0);
+//            motionDirection = sharedPreferences.getBoolean("motionDirection", false);
+//            myPresets.add(new MySliderPresets(name, time, travel));
         }
     }
 
@@ -88,12 +130,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             editor.putInt("presetTravel" + i, myPresets.get(i).getPresetTravel());
             editor.commit();
         }
+        editor.putBoolean("motionDirection", motionDirection);
+        editor.commit();
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        storePresetsInStorage();
+    protected void onStart() {
+        super.onStart();
+        defaultBackground = findViewById(R.id.btnHowToUse).getBackground();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //storePresetsInStorage();
     }
 
     @Override
@@ -121,15 +171,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch(v.getId()) {
-            case R.id.btnSettings: openSettingsActivity();
-            case R.id.btnHowToUse: openHowToUseActivity();
-            case R.id.btnAbout: openAboutActivity();
-            case R.id.btnBackwardMotion: setMotionToBackward();
-            case R.id.btnForwardMotion: setMotionToForward();
-            case R.id.btnSelectPresets: break; //Pop-Up Menu operations have to be done
-            case R.id.btnEditTime: openPopUpToEditTime();
-            case R.id.btnEditTravel: openPopUpToEditTravel();
-            case R.id.btnGo: startCameraSlider();
+            case R.id.btnSettings:
+                openSettingsActivity();
+                break;
+            case R.id.btnHowToUse:
+                openHowToUseActivity();
+                break;
+            case R.id.btnAbout:
+                openAboutActivity();
+                break;
+            case R.id.btnBackwardMotion:
+                setMotionToBackward();
+                break;
+            case R.id.btnForwardMotion:
+                setMotionToForward();
+                break;
+            case R.id.btnSelectPresets:
+                break; //Pop-Up Menu operations have to be done
+            case R.id.btnEditTime:
+                openPopUpToEditTime();
+                break;
+            case R.id.btnEditTravel:
+                openPopUpToEditTravel();
+                break;
+            case R.id.btnGo:
+                startCameraSlider();
+                break;
             default: break;
         }
     }
@@ -150,22 +217,121 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void setMotionToBackward() {
-
+        motionDirection = false; // Enable Backward Motion
+        btnBackwardMotion.setBackgroundColor(Color.GREEN);
+        btnForwardMotion.setBackground(defaultBackground);
     }
 
     private void setMotionToForward() {
-
+        motionDirection = true; // Enable Forward Motion
+        btnForwardMotion.setBackgroundColor(Color.GREEN);
+        btnBackwardMotion.setBackground(defaultBackground);
     }
 
     private void openPopUpToEditTime() {
+        final Dialog myTimeEditorDialog = new Dialog(MainActivity.this);
+        myTimeEditorDialog.setTitle("Edit Time");
+        myTimeEditorDialog.setContentView(R.layout.dialog_time_editor);
 
+        final NumberPicker hourPicker = (NumberPicker) myTimeEditorDialog.findViewById(R.id.hourPicker);
+        hourPicker.setMaxValue(11);
+        hourPicker.setMinValue(0);
+        hourPicker.setWrapSelectorWheel(false);
+
+        final NumberPicker minutePicker = (NumberPicker) myTimeEditorDialog.findViewById(R.id.minutePicker);
+        minutePicker.setMaxValue(59);
+        minutePicker.setMinValue(0);
+        minutePicker.setWrapSelectorWheel(false);
+
+        final NumberPicker secondPicker = (NumberPicker) myTimeEditorDialog.findViewById(R.id.secondPicker);
+        secondPicker.setMaxValue(59);
+        secondPicker.setMinValue(0);
+        secondPicker.setWrapSelectorWheel(false);
+
+        Button btnCancelEditingTime = (Button) myTimeEditorDialog.findViewById(R.id.btnCancelEditingTime);
+        btnCancelEditingTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myTimeEditorDialog.dismiss();
+            }
+        });
+
+        Button btnAcceptEditingTime = (Button) myTimeEditorDialog.findViewById(R.id.btnAcceptEditingTime);
+        btnAcceptEditingTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                time = String.valueOf(hourPicker.getValue())
+                        +String.valueOf(minutePicker.getValue())
+                        +String.valueOf(secondPicker.getValue());
+                etTime.setText(String.valueOf(hourPicker.getValue()) + "H "
+                        + String.valueOf(minutePicker.getValue()) + "M "
+                        + String.valueOf(secondPicker.getValue()) + "S");
+                myTimeEditorDialog.dismiss();
+            }
+        });
     }
 
     private void openPopUpToEditTravel() {
+        final Dialog myTravelEditorDialog = new Dialog(MainActivity.this);
+        myTravelEditorDialog.setTitle("Edit Travel");
+        myTravelEditorDialog.setContentView(R.layout.dialog_travel_editor);
 
+        final NumberPicker inchPicker = (NumberPicker) myTravelEditorDialog.findViewById(R.id.inchPicker);
+        inchPicker.setMaxValue(60);
+        inchPicker.setMinValue(1);
+        inchPicker.setWrapSelectorWheel(false);
+
+        Button btnCancelEditingTravel = (Button) myTravelEditorDialog.findViewById(R.id.btnCancelEditingTravel);
+        btnCancelEditingTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myTravelEditorDialog.dismiss();
+            }
+        });
+
+        Button btnAcceptEditingTravel = (Button) myTravelEditorDialog.findViewById(R.id.btnAcceptEditingTravel);
+        btnAcceptEditingTravel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                travel = String.valueOf(inchPicker.getValue());
+                etTravel.setText(String.valueOf(inchPicker.getValue())+" inch");
+                myTravelEditorDialog.dismiss();
+            }
+        });
     }
 
     private void startCameraSlider() {
+        sendDataOverBluetooth();
+    }
 
+    private void sendDataOverBluetooth() {
+
+    }
+
+    class BluetoothConnectivity {
+
+        public Context mContext;
+        BluetoothAdapter mBluetoothAdapter;
+
+        public BluetoothConnectivity(Context mContext) {
+            this.mContext = mContext;
+        }
+
+        public void connectBluetoothIfAvailable() {
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter == null) {
+                Toast.makeText(mContext, "Your device does not support Bluetooth", Toast.LENGTH_SHORT).show();
+            } else {
+                connectBluetooth();
+//                showListOfAvailableDevices(); http://examples.javacodegeeks.com/android/core/bluetooth/bluetoothadapter/android-bluetooth-example/
+            }
+        }
+
+        public void connectBluetooth() {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent,REQUEST_ENABLE_BT);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/rifat/sliderbot/Activities/SplashScreen.java
+++ app/src/main/java/com/example/rifat/sliderbot/Activities/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.example.rifat.sliderbot.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,9 +12,29 @@ import com.example.rifat.sliderbot.R;
 
 public class SplashScreen extends Activity {
 
+    boolean splashSeen = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2500);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(splashSeen) {
+            onBackPressed();
+        } else {
+            splashSeen = true;
+        }
     }
 }
